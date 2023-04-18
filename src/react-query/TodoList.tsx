06b4db3850480665282d1a3556251ed53fd0fa3a
reiorozco <rfoc15@gmail.com>
2023-04-18 18:51:44 -0500

    Creating a Custom Query Hook

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,28 +1,9 @@
 import React from "react";
-import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
 
-interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
+import useTodos from "./hooks/useTodos";
 
 const TodoList = () => {
-  const fetchTodos = () =>
-    axios
-      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.data);
-
-  const {
-    data: todos,
-    error,
-    isLoading,
-  } = useQuery<Todo[], Error>({
-    queryKey: ["todos"],
-    queryFn: fetchTodos,
-  });
+  const { data: todos, isLoading, error } = useTodos();
 
   if (isLoading) return <p>Loading...</p>;
 

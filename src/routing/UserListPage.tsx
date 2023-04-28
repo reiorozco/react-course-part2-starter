8f07b620ba9052771a6f2dd4b62e248cd493b4e2
reiorozco <rfoc15@gmail.com>
2023-04-28 16:11:34 -0500

    Navigation

--- src/routing/UserListPage.tsx
+++ src/routing/UserListPage.tsx
@@ -1,17 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 const UserListPage = () => {
   const users = [
-    { id: 1, name: 'Mosh' },
-    { id: 2, name: 'John' },
-    { id: 3, name: 'Alice' },
+    { id: 1, name: "Mosh" },
+    { id: 2, name: "John" },
+    { id: 3, name: "Alice" },
   ];
   return (
-    <ul className="list-group">
-      {users.map((user) => (
-        <li className="list-group-item" key={user.id}>
-          <a href="#">{user.name}</a>
-        </li>
-      ))}
-    </ul>
+    <>
+      <div className="ms-1 mb-2">
+        <Link to="/">Go back</Link>
+      </div>
+
+      <ul className="list-group">
+        {users.map((user) => (
+          <li className="list-group-item" key={user.id}>
+            <a href="#">{user.name}</a>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 

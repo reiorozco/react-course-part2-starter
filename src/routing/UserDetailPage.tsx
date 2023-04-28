6a2f7447d82a359e4e2a9163338793899e21e17a
reiorozco <rfoc15@gmail.com>
2023-04-28 16:45:57 -0500

    get data about the current route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 
 const UserDetailPage = () => {
   const { id: idUser } = useParams();
+  // const [searchParams, setSearchParams] = useSearchParams();
+  // const location = useLocation();
 
   return <p>User {idUser}</p>;
 };

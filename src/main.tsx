9433efd525f180c1b782f7643833e88911d7e5ed
reiorozco <rfoc15@gmail.com>
2023-04-28 16:03:48 -0500

    Set up routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router-dom";
 
-import App from "./App";
-
+import router from "./routing/routes";
 import "./index.css";
 
 const queryClient = new QueryClient();
@@ -13,7 +13,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
 
       <ReactQueryDevtools />
     </QueryClientProvider>
---
+++ src/routing/routes.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+import HomePage from "./HomePage";
+import UserListPage from "./UserListPage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/users", element: <UserListPage /> },
+]);
+
+export default router;
